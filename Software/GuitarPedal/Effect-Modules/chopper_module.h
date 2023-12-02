37ee7c2c9ebf20ecb95938dfe77c29eea6cc8b4a
Snail Mail <>
2023-12-02 14:04:25 -0600

    Add module header

--- Software/GuitarPedal/Effect-Modules/chopper_module.h
+++ Software/GuitarPedal/Effect-Modules/chopper_module.h
@@ -2,39 +2,40 @@
 #ifndef CHOPPER_MODULE_H
 #define CHOPPER_MODULE_H
 
-#include <stdint.h>
 #include "daisysp.h"
-#include "base_effect_module.h"
 #include "Chopper/chopper.h"
+#include "base_effect_module.h"
+#include <stdint.h>
 #ifdef __cplusplus
 
 /** @file chopper_module.h */
 
 using namespace daisysp;
 
-namespace bkshepherd
-{
+namespace bkshepherd {
 
-class ChopperModule : public BaseEffectModule
-{
-  public:
-    ChopperModule();
-    ~ChopperModule();
+class ChopperModule : public BaseEffectModule {
+public:
+  ChopperModule();
+  ~ChopperModule();
 
-    void Init(float sample_rate) override;
-    void ProcessMono(float in) override;
-    void ProcessStereo(float inL, float inR) override;
-    void SetTempo(uint32_t bpm) override;
-    float GetBrightnessForLED(int led_id) override;
+  void Init(float sample_rate) override;
+  void ProcessMono(float in) override;
+  void ProcessStereo(float inL, float inR) override;
+  void SetTempo(uint32_t bpm) override;
+  float GetBrightnessForLED(int led_id) override;
+  void DrawUI(OneBitGraphicsDisplay &display, int currentIndex,
+              int numItemsTotal, Rectangle boundsToDrawIn,
+              bool isEditing) override;
 
-  private:
-    Chopper m_chopper;
-    float m_tempoFreqMin;
-    float m_tempoFreqMax;
-    float m_pulseWidthMin;
-    float m_pulseWidthMax;
+private:
+  Chopper m_chopper;
+  float m_tempoFreqMin;
+  float m_tempoFreqMax;
+  float m_pulseWidthMin;
+  float m_pulseWidthMax;
 
-    float m_cachedEffectMagnitudeValue;
+  float m_cachedEffectMagnitudeValue;
 };
 } // namespace bkshepherd
 #endif
