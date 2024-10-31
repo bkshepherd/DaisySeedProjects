eceabc0a54c68b45540bb21a32e1ac7fe297083a
Sean Kelly <sevendaystillsean@gmail.com>
2024-10-30 20:38:42 -0700

    Make looper mono to have a smaller memory footprint due to the large buffer size

--- Software/GuitarPedal/Effect-Modules/looper_module.h
+++ Software/GuitarPedal/Effect-Modules/looper_module.h
@@ -6,48 +6,39 @@
 
 #include "../Util/looper.h"
 #include "base_effect_module.h"
-#include "daisysp.h"
 #ifdef __cplusplus
 
 /** @file looper_module.h */
 
-using namespace daisysp;
+namespace bkshepherd {
 
-namespace bkshepherd
-{
+class LooperModule : public BaseEffectModule {
+ public:
+  LooperModule();
+  ~LooperModule();
 
-class LooperModule : public BaseEffectModule
-{
-  public:
-    LooperModule();
-    ~LooperModule();
+  void Init(float sample_rate) override;
+  void ParameterChanged(int parameter_id) override;
+  void ProcessMono(float in) override;
+  void ProcessStereo(float inL, float inR) override;
+  float GetBrightnessForLED(int led_id) override;
+  bool AlternateFootswitchForTempo() const override { return false; }
+  void AlternateFootswitchPressed() override;
+  void AlternateFootswitchHeldFor1Second() override;
+  void DrawUI(OneBitGraphicsDisplay &display, int currentIndex,
+              int numItemsTotal, Rectangle boundsToDrawIn,
+              bool isEditing) override;
 
-    void Init(float sample_rate) override;
-    void ParameterChanged(int parameter_id) override;
-    void ProcessMono(float in) override;
-    void ProcessStereo(float inL, float inR) override;
-    float GetBrightnessForLED(int led_id) override;
-    bool AlternateFootswitchForTempo() const override
-    {
-        return false;
-    }
-    void AlternateFootswitchPressed() override;
-    void AlternateFootswitchHeldFor1Second() override;
-    void DrawUI(OneBitGraphicsDisplay &display, int currentIndex, int numItemsTotal, Rectangle boundsToDrawIn,
-                bool isEditing) override;
+ private:
+  void SetLooperMode();
 
-  private:
-    void SetLooperMode();
+  daisysp_modified::Looper m_looper;
 
-    // Loopers and the buffers they'll use
-    daisysp_modified::Looper m_looperL;
-    daisysp_modified::Looper m_looperR;
-
-    float m_inputLevelMin;
-    float m_inputLevelMax;
-    float m_loopLevelMin;
-    float m_loopLevelMax;
+  float m_inputLevelMin;
+  float m_inputLevelMax;
+  float m_loopLevelMin;
+  float m_loopLevelMax;
 };
-} // namespace bkshepherd
+}  // namespace bkshepherd
 #endif
 #endif
