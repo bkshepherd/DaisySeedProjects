6c19abc69319910e2963e61a26a1b17f83b9a96f
Sean Kelly <xconverge@users.noreply.github.com>
2025-03-04 21:16:20 -0800

    Make loaded effects a bit easier to manage (#63)
    * Make loaded effects a bit easier to manage (no renumbering)

--- Software/GuitarPedal/loaded_effects.h
+++ Software/GuitarPedal/loaded_effects.h
@@ -5,6 +5,9 @@
 #define LOADED_EFFECTS_H
 #pragma once
 
+#include "Effect-Modules/base_effect_module.h"
+
+// Include all effect modules
 #include "Effect-Modules/autopan_module.h"
 #include "Effect-Modules/chopper_module.h"
 #include "Effect-Modules/chorus_module.h"
@@ -18,12 +21,11 @@
 #include "Effect-Modules/overdrive_module.h"
 #include "Effect-Modules/peq_module.h"
 // #include "Effect-Modules/pitch_shifter_module.h" // Commented out to make room in DTCRAM
-#include "Effect-Modules/reverb_module.h"
-#include "Effect-Modules/tuner_module.h"
-
 #include "Effect-Modules/amp_module.h"
 #include "Effect-Modules/cloudseed_module.h" // Takes up significant SDRAM (about 30%)
 #include "Effect-Modules/nam_module.h"
+#include "Effect-Modules/reverb_module.h"
+#include "Effect-Modules/tuner_module.h"
 // #include "Effect-Modules/midi_keys_module.h"
 #include "Effect-Modules/delay_module.h"
 // #include "Effect-Modules/pluckecho_module.h"
@@ -32,46 +34,50 @@
 #include "Effect-Modules/polyoctave_module.h"
 #include "Effect-Modules/scifi_module.h"
 #include "Effect-Modules/spectral_delay_module.h"
-#include "Effect-Modules/spectral_delay_module.h"
-//#include "Effect-Modules/fm_keys_module.h"
+// #include "Effect-Modules/fm_keys_module.h"
 #include "Effect-Modules/granulardelay_module.h"
 
 void load_effects(int &availableEffectsCount, BaseEffectModule **&availableEffects) {
-    // Make sure this count matches the maxindex - 1 (count) of effects that get
-    // added to the array
-    availableEffectsCount = 23;
-    availableEffects = new BaseEffectModule *[availableEffectsCount];
-    availableEffects[0] = new ModulatedTremoloModule();
-    availableEffects[1] = new OverdriveModule();
-    availableEffects[2] = new AutoPanModule();
-    availableEffects[3] = new ChorusModule();
-    availableEffects[4] = new ChopperModule();
-    availableEffects[5] = new ReverbModule();
-    availableEffects[6] = new MultiDelayModule();
-    availableEffects[7] = new MetroModule();
-    availableEffects[8] = new TunerModule();
-    // availableEffects[9] = new PitchShifterModule();
-    availableEffects[9] = new CompressorModule();
-    availableEffects[10] = new LooperModule();
-    availableEffects[11] = new GraphicEQModule();
-    availableEffects[12] = new ParametricEQModule();
-    availableEffects[13] = new NoiseGateModule();
+    // clang-format off
+    static BaseEffectModule* effectList[] = {
+        new ModulatedTremoloModule(),
+        new OverdriveModule(),
+        new AutoPanModule(),
+        new ChorusModule(),
+        new ChopperModule(),
+        new ReverbModule(),
+        new MultiDelayModule(),
+        new MetroModule(),
+        new TunerModule(),
+        // new PitchShifterModule(),
+        new CompressorModule(),
+        new LooperModule(),
+        new GraphicEQModule(),
+        new ParametricEQModule(),
+        new NoiseGateModule(),
+        new CloudSeedModule(),
+        new AmpModule(),
+        new DelayModule(),
+        new NamModule(),
+        new SciFiModule(),
+        new PolyOctaveModule(),
+        new SpectralDelayModule(),
+        new DistortionModule(),
+        new GranularDelayModule(), 
 
-    availableEffects[14] = new CloudSeedModule();
-    availableEffects[15] = new AmpModule();
-    availableEffects[16] = new DelayModule();
-    availableEffects[17] = new NamModule();
-    availableEffects[18] = new SciFiModule();
-    availableEffects[19] = new PolyOctaveModule();
-    availableEffects[20] = new SpectralDelayModule();
-    availableEffects[21] = new DistortionModule();
-    availableEffects[22] = new GranularDelayModule(); 
-    // The following require a MIDI keyboard
-    // availableEffects[23] = new MidiKeysModule();
-    // availableEffects[24] = new PluckEchoModule();
-    // availableEffects[25] = new StringKeysModule();
-    // availableEffects[26] = new ModalKeysModule();
-    // availableEffects[22] = new FmKeysModule();
+        // The following require a MIDI keyboard
+        // new MidiKeysModule(),
+        // new PluckEchoModule(),
+        // new StringKeysModule(),
+        // new ModalKeysModule()
+    };
+    // clang-format on
+
+    availableEffectsCount = sizeof(effectList) / sizeof(effectList[0]);
+    availableEffects = new BaseEffectModule *[availableEffectsCount];
+    for (int i = 0; i < availableEffectsCount; ++i) {
+        availableEffects[i] = effectList[i];
+    }
 }
 
 #endif
