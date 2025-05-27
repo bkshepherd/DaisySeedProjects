e84364c54719b924c817c05b1ee29da49d477457
Sean Kelly <sevendaystillsean@gmail.com>
2025-05-26 18:43:37 -0700

    Add a few more NAM models

--- Software/GuitarPedal/Effect-Modules/nam_module.cpp
+++ Software/GuitarPedal/Effect-Modules/nam_module.cpp
@@ -19,8 +19,11 @@ cycfi::q::peaking filter_nam[NUM_FILTERS_NAM] = {{0, centerFrequencyNam[0], 4800
                                                  {0, centerFrequencyNam[1], 48000, q_nam[1]},
                                                  {0, centerFrequencyNam[2], 48000, q_nam[2]}};
 
-// static const char* s_modelBinNames[3] = {"HRClean", "MarshJVM", "Tumnus32k"};
-static const char *s_modelBinNames[8] = {"Mesa", "Match30", "DumHighG", "DumLowG", "Ethos", "Splawn", "PRSArch", "JCM800"};
+// This must match the length of the model_collection_nam array in model_data_nam.h
+const size_t k_numModels = 11;
+
+static const char *s_modelBinNames[k_numModels] = {"Mesa",    "Match30", "DumHighG",    "DumLowG",     "Ethos",      "Splawn",
+                                                   "PRSArch", "JCM800",  "BE-100(21a)", "BE-100(41a)", "JCM800(08c)"};
 struct NAMMathsProvider {
 #if RTNEURAL_USE_EIGEN
     template <typename Matrix> static auto tanh(const Matrix &x) {
@@ -49,7 +52,7 @@ static const ParameterMetaData s_metaData[s_paramCount] = {
     {
         name : "Model",
         valueType : ParameterValueType::Binned,
-        valueBinCount : 8,
+        valueBinCount : k_numModels,
         valueBinNames : s_modelBinNames,
         defaultValue : {.uint_value = 0},
         knobMapping : 2,
