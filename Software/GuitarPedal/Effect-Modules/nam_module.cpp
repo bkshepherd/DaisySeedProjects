eba4cd32694f1a046646bd49d90ab3d83fca2b34
Sean Kelly <xconverge@users.noreply.github.com>
2025-06-25 20:09:59 -0700

    Add Sansamp Bass Driver V1 Clean NAM model (#71)
    * Add sansamp BDDI to NAM module
    * Remove duplicate models to save space

--- Software/GuitarPedal/Effect-Modules/nam_module.cpp
+++ Software/GuitarPedal/Effect-Modules/nam_module.cpp
@@ -20,10 +20,11 @@ cycfi::q::peaking filter_nam[NUM_FILTERS_NAM] = {{0, centerFrequencyNam[0], 4800
                                                  {0, centerFrequencyNam[2], 48000, q_nam[2]}};
 
 // This must match the length of the model_collection_nam array in model_data_nam.h
-const size_t k_numModels = 12;
+const size_t k_numModels = 10;
 
-static const char *s_modelBinNames[k_numModels] = {"Mesa",    "Match30", "DumHighG",    "DumLowG",     "Ethos",       "Splawn",
-                                                   "PRSArch", "JCM800",  "JCM800(08c)", "BE-100(21a)", "BE-100(41a)", "BE-100(10)"};
+static const char *s_modelBinNames[k_numModels] = {
+    "Mesa", "Match30", "DumHighG", "DumLowG", "Ethos", "Splawn", "PRSArch", "JCM800", "SansAmp", "BE-100",
+};
 struct NAMMathsProvider {
 #if RTNEURAL_USE_EIGEN
     template <typename Matrix> static auto tanh(const Matrix &x) {
@@ -203,6 +204,11 @@ void NamModule::ProcessMono(float in) {
     if (GetParameterAsBool(6)) {
         ampOut =
             rtneural_wavenet.forward(input_arr[0]) * 0.4; // TODO Try this again, was sending the whole array, wants just the float
+
+        // Apply level normalization factor
+        if (m_currentModelindex >= 0 && m_currentModelindex < static_cast<int>(k_numModels)) {
+            ampOut *= model_collection_nam[m_currentModelindex].levelAdjust;
+        }
     } else {
         ampOut = input_arr[0];
     }
