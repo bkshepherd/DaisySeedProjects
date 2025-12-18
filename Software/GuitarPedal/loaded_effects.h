57280fd8871cfb7f1f2b7b3704e5e7dd52d2cda5
Nick Bennett <nicolascbennett@gmail.com>
2025-12-18 16:49:27 -0700

    Phase 1, 2 effects loaded, for debugging encoder

--- Software/GuitarPedal/loaded_effects.h
+++ Software/GuitarPedal/loaded_effects.h
@@ -4,12 +4,14 @@
 
 #include "Effect-Modules/base_effect_module.h"
 #include "Effect-Modules/modulated_tremolo_module.h"
+#include "Effect-Modules/chorus_module.h"
 
 namespace bkshepherd {
 
 void load_effects(int &availableEffectsCount, BaseEffectModule **&availableEffects) {
     static BaseEffectModule* effectList[] = {
         new ModulatedTremoloModule(),
+        new ChorusModule(),
     };
     
     availableEffectsCount = sizeof(effectList) / sizeof(effectList[0]);
