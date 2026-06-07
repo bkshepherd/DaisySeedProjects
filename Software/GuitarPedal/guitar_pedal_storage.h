f39028047fa267329562778e5b3e89c10c0fd5ec
Sean Kelly <sevendaystillsean@gmail.com>
2026-06-07 16:08:45 -0700

    Add ability to reboot to bootloader with the screen/UI/encoder so that I don't need to open the pedal

--- Software/GuitarPedal/guitar_pedal_storage.h
+++ Software/GuitarPedal/guitar_pedal_storage.h
@@ -56,5 +56,6 @@ void SaveEffectSettingsToPersitantStorageForEffectID(int effectID, uint32_t pres
 void SetSettingsParameterValueForEffect(int effectID, int paramID, uint32_t paramValue, uint32_t startIdx);
 void LoadPresetFromPersistentStorage(uint32_t effectID, uint32_t presetID);
 void FactoryReset(void *context);
+void RebootToBootloader(void *context);
 
 #endif
